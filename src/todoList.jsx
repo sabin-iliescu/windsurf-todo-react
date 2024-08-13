443eb9175ad5f0188ffd2d9617cf65895d2a7e58
Sabin Iliescu <sabin.iliescu@gmail.com>
2024-08-13 14:52:49 +0300

    add todos pagination

--- src/todoList.jsx
+++ src/todoList.jsx
@@ -1,8 +1,18 @@
+import { useState } from "react";
 import { TodoItem } from "./todoItem";
 import { useFilteredTodos } from "./hooks/useFilteredTodos";
 
 export function TodoList() {
   const { filteredTodos, setFilter } = useFilteredTodos();
+  const [currentPage, setCurrentPage] = useState(1);
+  const [todosPerPage, setTodosPerPage] = useState(5);
+
+  const totalPages = Math.ceil(filteredTodos.length / todosPerPage);
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+  const indexOfLastTodo = currentPage * todosPerPage;
+  const indexOfFirstTodo = indexOfLastTodo - todosPerPage;
+  const currentTodos = filteredTodos.slice(indexOfFirstTodo, indexOfLastTodo);
 
   return (
     <div className="card">
@@ -33,13 +43,29 @@ export function TodoList() {
         </div>
       </div>
       <ul className="list-group list-group-flush ps-2 pe-2 pt-2">
-        {filteredTodos.length === 0 && (
+        {currentTodos.length === 0 && (
           <h6 className="text-center m-3">No todos available</h6>
         )}
-        {filteredTodos.map((todo) => {
-          return <TodoItem key={todo.id} todo={todo} />;
-        })}
+        {currentTodos.map((todo) => (
+          <TodoItem key={todo.id} todo={todo} />
+        ))}
       </ul>
+      <nav className="d-flex justify-content-center mt-3">
+        <ul className="pagination">
+          {Array.from({ length: totalPages }, (_, index) => (
+            <li
+              key={index}
+              className={`page-item ${
+                currentPage === index + 1 ? "active" : ""
+              }`}
+            >
+              <a onClick={() => paginate(index + 1)} className="page-link">
+                {index + 1}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </div>
   );
 }
